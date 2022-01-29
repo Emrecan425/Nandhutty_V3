f90cddcd75f960c28084b0ca212b0665b96ca80e
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-01-29 19:23:22 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,12 +1,35 @@
 const { servers, yta, ytv } = require('../lib/y2mate')
 let yts = require('yt-search')
+let fs = require('fs')
 let fetch = require('node-fetch')
+const reply = {
+key: {
+			participant: '@s.whatsapp.net'
+		},
+		message: {
+			"productMessage": {
+				"product": {
+					"productImage":{
+						"mimetype": "image/jpeg",
+						"jpegThumbnail":  fs.readFileSync('./icon.jpeg') ////Gambarny
+					},
+					"title": `Nandhutty`, //Kasih namalu 
+					"description": `%name`, 
+					"currencyCode": "INR",
+					"priceAmount1000": "999999999",
+					"retailerId": ` %name`,
+					"productImageCount": 1
+				},
+				    "businessOwnerJid": `0@s.whatsapp.net`
+		}
+	}
+}
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} california`
+  if (!text) throw `uhm.. what are you looking for?\n\nexample:\n${usedPrefix + command} PaniPalli`
   let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Konten Tidak ditemukan'
+  if (!vid) throw 'Content Not found'
   let isVideo = /2$/.test(command)
   let yt = false
   let yt2 = false
@@ -19,24 +42,18 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
       usedServer = server
       break
     } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntry again...'}`)
     }
   }
-  if (yt === false) throw 'semua server gagal'
-  if (yt2 === false) throw 'semua server gagal'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
-*Server y2mate:* ${usedServer}
-`.trim(), '© stikerin', 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`)
+  await conn.send3ButtonImg(m.chat, await (await fetch(thumb)).buffer(), `*Title:* ${title}
+*Audio File Size:* ${filesizeF}
+*Video File Size:* ${yt2.filesizeF}`, watermark, `🎵 ᴍᴘ3 ${filesizeF}`, `.yta ${vid.url}`, `🎥 ᴍᴘ4 ${yt2.filesizeF}`, `.ytv ${vid.url}`, `▶️Source Url ${dl_link}`, '.sc', reply)
 }
-handler.help = ['play'].map(v => v + ' <pencarian>')
+handler.help = ['song','play','?'].map(v => v + ' <query>')
 handler.tags = ['downloader']
-handler.command = /^(p|play)$/i
+handler.command = /^(play|song)$/i
 
 handler.exp = 0
 
 module.exports = handler
-
