ff7b5217cc3c921886829ab9233cbe15f2be5239
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-05 13:07:44 +0530

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -1,22 +1,22 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+let handler = async(m, { conn, text, usedPrefix, command }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = db.data.users[who]
-    if (!who) return m.reply(`Tag/Mention!\n\nContoh:\n${usedPrefix + command} @0 1\n\nAngka 1 menunjukan total hari`)
+    if (!who) return m.reply(`Tag/Mention!\n\nExample:\n${usedPrefix + command} @0 1\n\nThe number 1 represents the total days`)
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw `Angkanya mana?\n\nContoh:\n${usedPrefix + command} @0 1`
-    if (isNaN(txt)) return m.reply(`Hanya angka!\n\nContoh:\n${usedPrefix + command} @0 1`)
-    var jumlahHari = 86400000 * txt
+    if (!txt) throw `Which number?\n\nExample:\n${usedPrefix + command} @0 1`
+    if (isNaN(txt)) return m.reply(`Only numbers!\n\nExample:\n${usedPrefix + command} @0 1`)
+    var number ofDays = 86400000 * txt
     var now = new Date() * 1
-    if (now < user.premiumTime) user.premiumTime += jumlahHari
-    else user.premiumTime = now + jumlahHari
+    if (now < user.premiumTime) user.premiumTime += number ofDays
+    else user.premiumTime = now + number ofDays
     user.premium = true
-    m.reply(`Berhasil menambahkan *${user.name}* sebagai pengguna Premium selama ${txt} hari.\n\nHitung mundur: ${conn.msToDate(user.premiumTime - now)}`)
+    m.reply(`Successfully added *${user.name}* as Premium user for ${txt} days.\n\nCountdown: ${conn.msToDate(user.premiumTime - now)}`)
 }
-handler.help = ['addprem [@user] <angka>']
+handler.help = ['addprem [@user] <number>']
 handler.tags = ['owner']
-handler.command = /^(add|tambah|\+)p(rem)?$/i
+handler.command = /^(add|add|\+)p(rem)?$/i
 
 handler.rowner = true
 
