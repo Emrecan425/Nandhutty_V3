fa744333c3b48703d5734e4946b563e0bafbc835
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-05 17:34:54 +0530

    Update caripesan.js

--- plugins/caripesan.js
+++ plugins/caripesan.js
@@ -1,21 +1,21 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Pengunaan:\n${usedPrefix + command} <pesan>|<jumlah pesan>\n\ncontoh:\n${usedPrefix + command} stikerin|5`
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `Usage:\n${usedPrefix + command} <message>|<number of messages>\n\nExample:\n${usedPrefix + command} sticker|5`
     let split = text.split`|`
     let result = await conn.searchMessages(split[0], m.chat, split[1] ? split[1] : 2, 1)
     if (result.messages.length > 0) {
         let total = result.messages.length
-        let sp = total < Number(split[1]) ? `Hanya ditemukan ${total} pesan` : `Ditemukan ${total} pesan`
+        let sp = total < Number(split[1]) ? `Only ${total} messages found` : `Found ${total} messages`
         m.reply(sp)
 
         result.messages.map(async ({ key }) => {
             let { remoteJid: _remoteJid, id: _ids } = key
             let _message = await conn.loadMessage(_remoteJid, _ids)
-            conn.reply(m.chat, 'Nih pesannya', _message)
+            conn.reply(m.chat, 'Here's the message', _message)
         })
     }
 }
-handler.help = ['caripesan <teks>|<angka>']
+handler.help = ['search for messages <text>|<number>']
 handler.tags = ['tools']
-handler.command = /^caripesan/i
+handler.command = /^searchmessage/i
 
 module.exports = handler
