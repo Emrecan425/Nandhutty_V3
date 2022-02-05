284d5cc53f71441af1379cb4980ddea9fa9fb9d1
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-05 15:06:53 +0530

    Update astagfir.js

--- plugins/astagfir.js
+++ plugins/astagfir.js
@@ -1,15 +1,15 @@
-let handler = async (m, { conn }) => {
+let handler = async(m, { conn }) => {
     let user = db.data.users[m.sender]
-    if (user.warning == 0) throw 'Kamu tidak memiliki warning!'
+    if (user.warning == 0) throw 'You have no warning!'
 
-    let waktu = user.lastIstigfar + 180000
-    if (new Date - user.lastIstigfar < 180000) throw `Kamu bisa menggunakan perintah ini lagi setelah ${conn.msToTime(waktu - new Date())}`
+    let time = user.lastIstigfar + 180000
+    if (new Date - user.lastIstigfar < 180000) throw `You can use this command again after ${conn.msToTime(time - new Date())}`
     user.warning -= 1
     m.reply(`Warning: ${user.warning} / 5`)
     user.lastIstigfar = new Date * 1
 }
-handler.command = /^(astagh?fir(ullah)?|maaf)$/i
+handler.command = /^(astagh?fir(ullah)?|sorry)$/i
 
 handler.limit = 1
 
-module.exports = handler 
+module.exports = handler
