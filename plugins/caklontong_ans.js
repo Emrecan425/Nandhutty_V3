151258416cbb732561225426a2873c39c3404d95
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-05 17:30:09 +0530

    Update caklontong_ans.js

--- plugins/caklontong_ans.js
+++ plugins/caklontong_ans.js
@@ -5,9 +5,9 @@ let handler = m => m
 
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*calo/i.test(m.quoted.contentText)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*calo/i.test(m.quoted.contentText)) return !0
     this.caklontong = this.caklontong ? this.caklontong : {}
-    if (!(id in this.caklontong)) return m.reply('Cak Lontong telah berakhir')
+    if (!(id in this.caklontong)) return m.reply('Cak Lontong has ended')
     if (m.quoted.id == this.caklontong[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.caklontong[id][1]))
         if (['.calo', ''].includes(m.text)) return !0
@@ -16,8 +16,8 @@ handler.before = async function (m) {
             await this.sendButton(m.chat, benar + ` +${this.caklontong[id][2]} XP\n${json.deskripsi}`, wm, 'Cak Lontong', '.caklontong', m)
             clearTimeout(this.caklontong[id][3])
             delete this.caklontong[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(dikit)
-        else m.reply(salah)
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(little)
+        else m.reply(wrong)
     }
     return !0
 }
