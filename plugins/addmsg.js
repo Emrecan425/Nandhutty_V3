c63a8466b229c46a6f7631b8d46fa85c57f05bd7
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-05 13:06:04 +0530

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -1,21 +1,22 @@
+
 let { WAMessageProto } = require('@adiwajshing/baileys')
 
-let handler = async (m, { conn, command, usedPrefix, text }) => {
+let handler = async(m, { conn, command, usedPrefix, text }) => {
     let M = WAMessageProto.WebMessageInfo
     let which = command.replace(/add/i, '')
-    if (!m.quoted) throw `Balas pesan dengan perintah *${usedPrefix + command}*`
-    if (!text) throw `Pengunaan:${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} tes`
+    if (!m.quoted) throw `Reply message with command *${usedPrefix + command}*`
+    if (!text) throw `Usage:${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} test`
     let msgs = db.data.msgs
-    if (text in msgs) throw `'${text}' telah terdaftar!`
+    if (text in msgs) throw `'${text}' has been registered!`
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
-    if (db.data.chats[m.chat].getmsg) return m.reply(`Berhasil menambahkan pesan '${text}'
+    if (db.data.chats[m.chat].getmsg) return m.reply(`Successfully added message '${text}'
     
-Akses dengan mengetik namanya`.trim())
-    else return await conn.sendButton(m.chat, `Berhasil menambahkan pesan '${text}'
+Access it by typing its name`.trim())
+    else return await conn.sendButton(m.chat, `Successfully added message '${text}'
     
-akses dengan ${usedPrefix}get${which} ${text}`, '© stikerin', 'Aktifkan', '.on getmsg', m)
+access with ${usedPrefix}get${which} ${text}`, '© sticker', 'Activate', '.on getmsg', m)
 }
-handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'stiker', 'gif'].map(v => 'add' + v + ' <teks>')
+handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'stickers', 'gif'].map(v => 'add' + v + ' <text>' )
 handler.tags = ['database']
 handler.command = /^add(vn|msg|video|audio|img|stic?ker|gif)$/
 
