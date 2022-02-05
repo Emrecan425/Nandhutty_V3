644dbaeb3500e82e9e4ab721777b2fab94ad0878
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-05 17:31:05 +0530

    Update caklontong_hint.js

--- plugins/caklontong_hint.js
+++ plugins/caklontong_hint.js
@@ -1,10 +1,10 @@
-let handler = async (m, { conn }) => {
+let handler = async(m, { conn }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
     if (!(id in conn.caklontong)) throw false
     let json = conn.caklontong[id][1]
-    let clue = json.jawaban.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\n\nbalas soalnya, bukan pesan ini!', conn.caklontong[id][0])
+    let clue = json.answer.replace(/[AIUEOaiueo]/g, '_')
+    conn.reply(m.chat, '```' + clue + '```\n\nReply the question, not this message!', conn.caklontong[id][0])
 }
 handler.command = /^calo$/i
 
