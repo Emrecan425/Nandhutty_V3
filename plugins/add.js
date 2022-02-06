f7ad5853ddb00d21de32e0824092204ed4a71407
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-06 19:15:56 +0530

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -1,10 +1,6 @@
 let fetch = require('node-fetch')
-
-let handler = async(m, { conn, text, participants, usedPrefix, command }) => {
-  if (m.quoted) {
-    await conn.groupAdd(m.chat, [m.quoted.sender]).catch(_ => _)
-  }
-  if (!text) throw `uhm.. what is the number?\n\nexample:\n\n${usedPrefix + command + ' ' + global.owner[0]}`
+let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
+  if (!text) throw `*ᴡʜɪᴄʜ ɴᴜᴍʙᴇʀ?* \n *ᴇxᴀᴍᴘʟᴇ:* \n\n${usedPrefix + command + ' ' + global.owner[0]}`
   let _participants = participants.map(user => user.jid)
   let users = (await Promise.all(
     text.split(',')
@@ -16,32 +12,31 @@ let handler = async(m, { conn, text, participants, usedPrefix, command }) => {
       ])
   )).filter(v => v[1]).map(v => v[0] + '@c.us')
   let response = await conn.groupAdd(m.chat, users)
-  if (response[users] == 408) throw `_Failed!_\n\nThe number has been logged out recently\nOnly login via *${usedPrefix}link* group`
+  if (response[users] == 408) throw `_Failed!_\n\nThe number has been dialed recently\nCan only enter via *${usedPrefix}link* grup`
   let pp = await conn.getProfilePicture(m.chat).catch(_ => false)
-  let jpegThumbnail = pp ? await(await fetch(pp)).buffer() : false
+  let jpegThumbnail = pp ? await (await fetch(pp)).buffer() : false
   for (let user of response.participants.filter(user => Object.values(user)[0].code == 403)) {
     let [[jid, {
       invite_code,
       invite_code_exp
     }]] = Object.entries(user)
-    let text = `Invite @${jid.split`@`[0]} using invite...`
-    m.reply(text, null, {
+    let teks = `*ɪɴᴠɪᴛɪɴɢ* @${jid.split`@`[0]} *ᴜꜱɪɴɢ ɪɴᴠɪᴛᴀᴛɪᴏɴ...*`
+    m.reply(teks, null, {
       contextInfo: {
-        mentionedJid: conn.parseMention(text)
+        mentionedJid: conn.parseMention(teks)
       }
     })
-    await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, false, 'Invitation to join my WhatsApp group', jpegThumbnail ? {
+    await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, false, 'ɪɴᴠɪᴛᴀᴛɪᴏɴ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴡʜᴀᴛꜱᴀᴘᴘ ɢʀᴏᴜᴘ', jpegThumbnail ? {
       jpegThumbnail
-    } : { })
+    } : {})
   }
 }
-handler.help = ['add', '+'].map(v => v + 'number,number')
-handler.tags = ['admin']
-handler.command = /^(add|\+)$/i
+handler.help = ['add', '+'].map(v => 'o' + v + ' @user')
+handler.tags = ['owner']
+handler.command = /^add/i
 
-handler.group = true
 handler.admin = true
+handler.group = true
 handler.botAdmin = true
-handler.limit = true
 
 module.exports = handler
