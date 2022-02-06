3dff72331927d57f15b71bce969c5c1e80c148f5
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-06 07:18:43 +0530

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -240,27 +240,27 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
       break
     default:
       if (!/[01]/.test(command)) throw `
-┌「 *Daftar Opsi* 」${isOwner ? '\n├ anon\n├ antispam\n├ antitroli\n├ autoread\n├ backup\n├ clear\n├ grouponly\n├ jadibot\n├ nsfw\n├ public\n├ mycontact' : ''}
+┌「 *List Of Options* 」${isOwner ? '\n├ anon\n├ antispam\n├ antitroli\n├ autoread\n├ backup\n├ clear\n├ grouponly\n├ jadibot\n├ nsfw\n├ public\n├ mycontact' : ''}
 ├ antilink
 ├ autolevelup
 ├ delete
 ├ detect
 ├ document
-├ stiker
+├ sticker
 ├ simi
 ├ welcome
 └────
-contoh:
+Example:
 ${usedPrefix}on welcome
 ${usedPrefix}off welcome
 `.trim()
       throw false
   }
   m.reply(`
-*${type}* berhasil di *${isEnable ? 'nyala' : 'mati'}kan* ${isAll ? 'untuk bot ini' : isUser ? '' : 'untuk chat ini'}
+*${type}* worked on *${isEnable ? 'on' : 'off'}right* ${isAll ? 'for this bot' : isUser ? '' : 'for this chat'}
 `.trim())
 }
-handler.help = ['on', 'off'].map(v => v + ' <opsi>')
+handler.help = ['on', 'off'].map(v => v + ' <options>')
 handler.tags = ['group', 'owner']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
 
