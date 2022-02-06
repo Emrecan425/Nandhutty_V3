6bd4ef9ddf4f7f49dc174db2a03861e53826e75b
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-06 14:40:40 +0530

    Update expired.js

--- plugins/expired.js
+++ plugins/expired.js
@@ -1,18 +1,18 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <angka>\n\nexample:\n${usedPrefix + command} 30\n\nAngka menunjukan total hari`
+let handler = async(m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Usage:\n${usedPrefix + command} <number>\n\nexample:\n${usedPrefix + command} 30\n\nNumber shows total days`
 
     let who
     if (m.isGroup) who = args[1] ? args[1] : m.chat
     else who = args[1]
 
-    var jumlahHari = 86400000 * args[0]
+    var number ofDays = 86400000 * args[0]
     var now = new Date() * 1
     let chat = db.data.chats[who]
-    if (now < chat.groupTime) chat.groupTime += jumlahHari
-    else chat.groupTime = now + jumlahHari
-    m.reply(`Hitung mundur: ${conn.msToDate(chat.groupTime - now)}`)
+    if (now < chat.groupTime) chat.groupTime += number of Days
+    else chat.groupTime = now + number ofDays
+    m.reply(`Countdown: ${conn.msToDate(chat.groupTime - now)}`)
 }
-handler.help = ['addgroup <angka> [jid]']
+handler.help = ['addgroup <number> [jid]']
 handler.tags = ['owner']
 handler.command = /^((\+|add)g(roup)?)$/i
 
