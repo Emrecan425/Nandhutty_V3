b3f93f0b9a2214b13816b9544ec0aebbac7a3970
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-06 14:47:29 +0530

    Update family100.js

--- plugins/family100.js
+++ plugins/family100.js
@@ -5,27 +5,27 @@ async function handler(m) {
     this.game = this.game ? this.game : {}
     let id = 'family100_' + m.chat
     if (id in this.game) {
-        this.sendButton(m.chat, 'Masih ada kuis yang belum terjawab!', wm, 'Nyerah', 'nyerah', this.game[id].msg)
+        this.sendButton(m.chat, 'There are still unanswered quizzes!', wm, 'Give up', 'give up', this.game[id].msg)
         throw false
     }
-    let res = await fetch(API('amel', '/family100', {}, 'apikey'))
-    if (!res.ok) throw eror
+    let res = await fetch(API('amel', '/family100', { }, 'apikey'))
+    if (!res.ok) throws error
     let json = await res.json()
     if (!json.status) throw json
     let caption = `
-*soal:* ${json.soal}
+*question:* ${json.question}
 
-terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
-(beberapa jawaban terdapat spasi)
+there are *${json.answers.length}* answers${json.answers.find(v => v.includes(' ')) ? `
+(some answers have spaces)
 
-+500 XP tiap jawaban benar
++500 XP each correct answer
 `: ''}
     `.trim()
     this.game[id] = {
-        id,
+        en,
         msg: await this.sendButton(m.chat, caption, wm, 'nyerah', 'nyerah', m),
         ...json,
-        terjawab: Array.from(json.jawaban, () => false),
+        answered: Array.from(json.answer, () => false),
         winScore,
     }
 }
