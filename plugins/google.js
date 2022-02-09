aa52ddec4a187ea27ca7433d0edf7ebd54a9e050
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-09 07:57:13 +0530

    Update google.js

--- plugins/google.js
+++ plugins/google.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let googleIt = require('google-it')
 
-let handler = async (m, { conn, command, args, usedPrefix }) => {
+let handler = async(m, { conn, command, args, usedPrefix }) => {
   let full = /f$/i.test(command)
   let text = args.join` `
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} Bahasa pemrograman`
+  if (!text) throw `uhm.. what are you looking for?\n\nexample:\n${usedPrefix + command} Programming language`
   let url = 'https://google.com/search?q=' + encodeURIComponent(text)
   let search = await googleIt({ query: text })
   let msg = search.map(({ title, link, snippet }) => {
@@ -14,11 +14,11 @@ let handler = async (m, { conn, command, args, usedPrefix }) => {
     let ss = await (await fetch(API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
     if (ss.includes('html')) throw ''
     await conn.sendFile(m.chat, ss, 'screenshot.png', url + '\n\n' + msg, m, 0, { thumbnail: await (await fetch(ss)).buffer() })
-  } catch (e) {
+  } catch(e) {
     m.reply(msg)
   }
 }
-handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
+handler.help = ['google', 'googlef'].map(v => v + ' <search>')
 handler.tags = ['internet']
 handler.command = /^googlef?$/i
 
