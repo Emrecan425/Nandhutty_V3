0295f544f1797ea6fa3d2ec7145d7d99b4e67d1c
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-09 07:54:10 +0530

    Update gimage.js

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -3,15 +3,15 @@ let _gis = require('g-i-s')
 let gis = promisify(_gis)
 let fetch = require('node-fetch')
 
-let handler = async (m, { conn, text, command, usedPrefix }) => {
-  if (!text) throw `Pengunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} pisang`
+let handler = async(m, { conn, text, command, usedPrefix }) => {
+  if (!text) throw `Usage:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} banana`
   let results = await gis(text) || []
   let { url, width, height } = conn.pickRandom(results) || {}
   if (!url) throw '404 Not Found'
   conn.sendFile(m.chat, url, 'gimage', '', m, 0, { thumbnail: await (await fetch(url)).buffer() })
 }
-handler.help = ['gimage <pencarian>', 'image <pencarian>']
+handler.help = ['gimage <search>', 'image <search>']
 handler.tags = ['internet']
 handler.command = /^(g?image)$/i
 
-module.exports = handler 
+module.exports = handler
