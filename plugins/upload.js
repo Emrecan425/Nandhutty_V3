9e6c5e28d7e7281793b36e06b96e34290059b33e
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-12 15:21:19 +0530

    Update upload.js

--- plugins/upload.js
+++ plugins/upload.js
@@ -1,16 +1,16 @@
 const uploadFile = require('../lib/uploadFile')
 const uploadImage = require('../lib/uploadImage')
 
-let handler = async (m, { usedPrefix, command }) => {
+let handler = async(m, { usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Balas media dengan perintah *${usedPrefix + command}*`
+  if (!mime) throw `Reply media with command *${usedPrefix + command}*`
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(media)
+  let link = await(isTele ? uploadImage : uploadFile)(media)
   m.reply(`${link}
 ${media.length} Byte(s)
-${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Tidak diketahui)'}`)
+${isTele ? '(No Expiration Date)' : '(Unknown)'}`)
 }
 handler.help = ['upload']
 handler.tags = ['tools']
