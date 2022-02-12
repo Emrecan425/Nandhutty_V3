83da11127aede863000084935d3a77f4ec09d22e
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-02-12 15:23:42 +0530

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,19 +1,25 @@
 let handler = async (m, { conn, isOwner, text, isAdmin }) => {
   let who
   if (m.isGroup) {
-    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+    if (!(isAdmin || isOwner)) {
+      global.dfail('admin', m, conn)
+      throw false
+    }
     who = m.mentionedJid[0] ? m.mentionedJid[0] : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
     // else who = m.chat
   } else {
-    if (!isOwner) return dfail('owner', m, conn)
+    if (!isOwner) {
+      global.dfail('owner', m, conn)
+      throw false
+    }
     who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
   }
   try {
-    if (who.endsWith('g.us')) db.data.chats[who].isBanned = false
-    else db.data.users[who].banned = false
-    m.reply(`${conn.user.name} sekarang aktif dichat ${conn.getName(who) == undefined ? 'ini' : conn.getName(who)}.`)
+    if (who.endsWith('g.us')) global.db.data.chats[who].isBanned = false
+    else global.db.data.users[who].banned = false
+    m.reply(`${conn.user.name} active chat ${conn.getName(who) == undefined ? 'ini' : conn.getName(who)}.`)
   } catch (e) {
-    throw `nomor tidak ada di database!`
+    throw `number does not exist in the database!`
   }
 }
 handler.help = ['unban']
