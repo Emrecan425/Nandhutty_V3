bd1d612621d891418a5cdb5eae091a092c25200a
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-02-13 12:43:57 +0530

    Update mention.js

--- plugins/mention.js
+++ plugins/mention.js
@@ -1,14 +1,15 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `contoh:\n${usedPrefix + command} @6285157336614`
-  m.reply(text, false, {
-    contextInfo: {
-      mentionedJid: conn.parseMention(text)
-    }
-  })
-}
-handler.help = ['mention <teks>']
-handler.tags = ['tools']
-
-handler.command = /^mention$/i
-
-module.exports = handler
+    if (!text) throw `example:\n${usedPrefix + command} @6285157336614`
+    m.reply(text, false, {
+      contextInfo: {
+        mentionedJid: conn.parseMention(text)
+      }
+    })
+  }
+  handler.help = ['mention <text>']
+  handler.tags = ['tools']
+  
+  handler.command = /^mention$/i
+  
+  module.exports = handler
+  
