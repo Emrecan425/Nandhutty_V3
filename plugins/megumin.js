554332d7b431c550b3c4bc5a8a404b55dc2d0f0d
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-02-13 12:32:52 +0530

    Update megumin.js

--- plugins/megumin.js
+++ plugins/megumin.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/megumin')
   if (!res.ok) throw eror
   let json = await res.json()
-  if (!json.url) throw 'Eror!'
-  conn.sendFile(m.chat, json.url, '', '© stikerin', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', '© sticker', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
 }
 handler.help = ['megumin']
 handler.tags = ['internet']
