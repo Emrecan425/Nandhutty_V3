a540b3b93e7020fe0232c299c34ef87206c0797b
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-02-13 12:06:03 +0530

    Update katabijak.js

--- plugins/katabijak.js
+++ plugins/katabijak.js
@@ -1,38 +1,38 @@
 const { quotes } = require('../lib/scrape')
 let handler = async (m, { command, args, usedPrefix }) => {
-    let er = `Pengunaan:
-${usedPrefix + command} <teks>
+    let er = `Use:
+${usedPrefix + command} <text>
 
-Contoh:
-${usedPrefix + command} cinta
+Example:
+${usedPrefix + command} love
 
-┌「 *Pilihan* 」
-├ cinta
-├ rindu
-├ mimpi
-├ sendiri
-├ sabar
-├ kesedihan
-├ pernikahan
-├ kemerdekaan
+┌「 *Options* 」
+├ love
+├ miss
+├ dream
+├ alone
+├ bepatient
+├ sadness
+├ wedding
+├ independence
 └────`.trim()
     if (!args[0]) throw er
     switch (args[0].toLowerCase()) {
-        case 'cinta':
-        case 'rindu':
-        case 'mimpi':
-        case 'sendiri':
-        case 'sabar':
-        case 'kesedihan':
-        case 'pernikahan':
-        case 'kemerdekaan':
+        case 'love':
+        case 'miss':
+        case 'dream':
+        case 'alone':
+        case 'patient':
+        case 'sadness':
+        case 'wedding':
+        case 'independence':
             quotes(args[0].toLowerCase()).then(async res => {
                 let data = JSON.stringify(res)
                 let json = JSON.parse(data)
                 let random = Math.floor(Math.random() * json.data.length)
                 let hasil = json.data[random]
                 let { author, bio, quote } = hasil
-                await conn.send2Button(m.chat, `“${quote}”`, `${author} - ${bio}`, `KATA BIJAK ${args[0].toUpperCase()}`, `${usedPrefix + command} ${args[0]}`, `Acak`, `${usedPrefix + command} ${conn.pickRandom(['rindu', 'mimpi', 'sendiri', 'sabar', 'kesedihan', 'pernikahan', 'kemerdekaan'])}`, m)
+                await conn.send2Button(m.chat, `“${quote}”`, `${author} - ${bio}`, `WORDS OF WISDOM ${args[0].toUpperCase()}`, `${usedPrefix + command} ${args[0]}`, `Random`, `${usedPrefix + command} ${conn.pickRandom(['miss', 'dream', 'alone', 'patient', 'sadness', 'marriage', 'independence'])}`, m)
             })
             break
         default:
