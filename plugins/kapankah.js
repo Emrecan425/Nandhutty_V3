c26a902fc919d511d92ae8e546b1cb2c6fbb6114
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-02-13 11:52:55 +0530

    Update kapankah.js

--- plugins/kapankah.js
+++ plugins/kapankah.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   m.reply(`
-*Pertanyaan:* ${m.text}
-*Jawaban:* ${Math.floor(Math.random() * 10)} ${conn.pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'])} lagi ...
+*Question:* ${m.text}
+*Answer:* ${Math.floor(Math.random() * 10)} ${conn.pickRandom(['second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'decade', 'century'])} again ...
 `.trim())
 }
 handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <teks>?')
