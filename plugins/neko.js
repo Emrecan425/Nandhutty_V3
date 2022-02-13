fbcd3c3d94496f38c377597d84f45c97fd576ded
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-02-13 12:49:42 +0530

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/neko')
   if (!res.ok) throw eror
   let json = await res.json()
-  if (!json.url) throw 'Eror!'
+  if (!json.url) throw 'Error!'
   conn.sendFile(m.chat, json.url, '', '© stikerin', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
 }
 handler.help = ['neko']
