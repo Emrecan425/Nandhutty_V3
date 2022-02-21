ee46fec1a4e497c8e6901112f8a1af4d9926b3f5
I_AM_DEVIL <88338865+D-3-V-1-L@users.noreply.github.com>
2022-02-21 19:41:00 +0530

    Update sendquote.js

--- plugins/sendquote.js
+++ plugins/sendquote.js
@@ -1,7 +1,7 @@
 async function handler(m) {
-    if (!m.quoted) throw `Balas pesan dengan perintah *${usedPrefix + command}*`
+    if (!m.quoted) throw `Reply to messages with commands *${usedPrefix + command}*`
     let q = this.serializeM(await m.getQuotedObj())
-    if (!q.quoted) throw 'Pesan yang anda balas tidak mengandung balasan!'
+    if (!q.quoted) throw 'The message you replied to did not contain a reply!'
     await q.quoted.copyNForward(m.chat, true)
 }
 handler.help = ['q']
