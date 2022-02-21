b1e1b3d899c9df69f9bd4a6ab33c1f411286b39a
Arjun-ser-ofc <99907222+Arjun-ser-ofc@users.noreply.github.com>
2022-02-21 23:25:14 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let haruno = `┌ 「 𝐆𝐈𝐓」
     sᴏʀʀʏ ʙʀᴏ ᴡᴇ ʜᴀᴠᴇɴ'ᴛ ʀᴇʟᴇᴀsᴇᴅ ᴛʜɪs ʙᴏᴛ 🙂
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/bf3338fe831a6ea2dfbdc.jpg")).buffer(), haruno, 'watermark', 'ᴍᴇɴᴜ', '.menu', 'ᴏᴡɴᴇʀ', '.owner', m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/Arjun-ser-ofc/Arjunser/main/Millie/20220204_171905.jpg")).buffer(), haruno, 'watermark', 'ᴍᴇɴᴜ', '.menu', 'ᴏᴡɴᴇʀ', '.owner', m)
 }
 handler.tags = ['main']
 handler.help = ['git']
